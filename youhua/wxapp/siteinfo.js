e92976275ac3266504e08784135be8507b05c2e8
Cyy <cyy1079276272@163.com>
2018-05-10 13:58:09 +0800

    youhua init

---
+++ youhua/wxapp/siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "左歌科技提供",
+    uniacid: "4",
+    acid: "4",
+    multiid: "0",
+    version: "1.0",
+    siteroot: "https://wjm.5188youxi.com/app/index.php",
+    design_method: "3"
+};
